9548bf4d96476ac0f49b232cdfb3d0fad27f3c8c
Juan Saint Clair <juan.pimentel@wooza.com.br>
2018-07-14 23:53:24 -0300

    criada function para definir a cor da temperatura de acordo com parametro passado

--- src/components/weather-block/weather-block.component.js
+++ src/components/weather-block/weather-block.component.js
@@ -16,5 +16,18 @@
     /* @ngInject */
     function WeatherBlockComponent() {
         var vm = this;
+
+        vm.setTemperatureColor = setTemperatureColor;
+
+        function setTemperatureColor(temperature) {
+            switch(true) {
+                case (temperature <= 5):
+                    return 'low';
+                case (temperature > 5 && temperature <= 25):
+                    return 'normal';
+                case (temperature > 25):
+                    return 'high';
+            }
+        }
     }
 })();
--- src/components/weather-block/weather-block.spec.js
+++ src/components/weather-block/weather-block.spec.js
@@ -8,10 +8,27 @@ describe('Component: WeatherBlock', () => {
 
     beforeEach(inject((_$componentController_, _$rootScope_) => {
         $ctrl = _$componentController_;
-        WeatherBlockComponent = $ctrl('caHeader', null, {});
+        WeatherBlockComponent = $ctrl('caWeatherBlock', null, {});
+        spyOn(WeatherBlockComponent, 'setTemperatureColor').and.callThrough();
+
     }));
     
     it('WeatherBlockComponent should be defined', () => {
         expect(WeatherBlockComponent).toBeDefined();
+    });
+
+    it('setTemperatureColor should return low', () => {
+        var temperatureClass = WeatherBlockComponent.setTemperatureColor(4);
+        expect(temperatureClass).toBe('low');
+    });
+
+    it('setTemperatureColor should return normal', () => {
+        var temperatureClass = WeatherBlockComponent.setTemperatureColor(16);
+        expect(temperatureClass).toBe('normal');
+    });
+
+    it('setTemperatureColor should return high', () => {
+        var temperatureClass = WeatherBlockComponent.setTemperatureColor(26);
+        expect(temperatureClass).toBe('high');
     });
 });
