e105ff122e43227d7fab857390b3e4829d9f1331
Juan Saint Clair <juan.pimentel@wooza.com.br>
2018-07-15 20:08:35 -0300

    criados os testes para main controller

--- src/pages/main/main.spec.js
+++ src/pages/main/main.spec.js
@@ -9,9 +9,16 @@ describe('Page: Main', () => {
     beforeEach(inject((_$controller_, _$rootScope_) => {
         $ctrl = _$controller_;
         MainController = $ctrl('MainController', null, {});
+
+        spyOn(MainController, 'changeSelectedCity').and.callThrough();
     }));
     
     it('MainController should be defined', () => {
         expect(MainController).toBeDefined();
+    });
+
+    it('changeSelectedCity should return city passed in parameter', () => {
+        MainController.changeSelectedCity('Rio de Janeiro');
+        expect(MainController.featuredCity).toBe('Rio de Janeiro');
     });
 });
